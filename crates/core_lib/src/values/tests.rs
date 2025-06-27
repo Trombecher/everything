c2a0625a53afbee8412015d2e8fc0affa6276f9c
Tobias Hillemanns <trombecher@icloud.com>
2025-06-27 05:13:14 +0200

    Rename core lib

--- crates/core_lib/src/values/tests.rs
+++
@@ -1,3 +0,0 @@
-#![cfg(test)]
-
-// TODO: encoding / decoding tests
--- crates/core_lib/src/versioning.rs
+++
@@ -1,5 +0,0 @@
-//! This module handles versioning. It contains the current [VERSION] of the source.
-//! 
-//! In the future it will contain migration code.
-
-pub const VERSION: u32 = 1;
