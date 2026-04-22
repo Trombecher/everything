43447a5f63da1f5777fea46f03f8623335f65870
Tobias Hillemanns <trombecher@icloud.com>
2026-04-22 20:06:30 +0200

    ...

--- crates/everything_structures/src/lib.rs
+++ crates/everything_structures/src/lib.rs
@@ -4,13 +4,11 @@
 //! read README
 
 mod abstracts;
-mod fixed_or_more;
 mod objects;
 mod properties;
 mod structures;
 
 pub use abstracts::*;
-pub use fixed_or_more::*;
 pub use objects::*;
 pub use properties::*;
 pub use structures::*;
