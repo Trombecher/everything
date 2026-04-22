2910596ba9be0d9ba53567991fe5aa60312749e0
Tobias Hillemanns <trombecher@icloud.com>
2026-04-22 20:15:08 +0200

    add `Object::exact_natural_number` test

--- crates/everything_structures/src/objects/tests.rs
+++ crates/everything_structures/src/objects/tests.rs
@@ -16,3 +16,27 @@ fn new_natural_number() {
         );
     }
 }
+
+#[test]
+fn exact_natural_number() {
+    assert_eq!(
+        Object::Abstract(Abstract::ZERO).exact_natural_number(),
+        Some(0)
+    );
+
+    assert_eq!(
+        Object::Abstract(Abstract(347539486456)).exact_natural_number(),
+        None
+    );
+
+    assert_eq!(
+        Object::Structure(Structure::NaturalNumber(NonZeroU128::new(10).unwrap()))
+            .exact_natural_number(),
+        Some(10)
+    );
+
+    assert_eq!(
+        Object::Structure(Structure::Character('x')).exact_natural_number(),
+        None
+    );
+}
