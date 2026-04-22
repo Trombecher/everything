ef8ad245900bdcdf8e45d452c5461f214abe8974
Tobias Hillemanns <trombecher@icloud.com>
2026-04-22 23:48:17 +0200

    more tests

--- crates/everything_structures/src/structures/bytes/tests.rs
+++ crates/everything_structures/src/structures/bytes/tests.rs
@@ -1,28 +1,73 @@
-use std::sync::Arc;
+use std::{num::NonZeroU128, sync::Arc};
 
-use crate::{BytesStructure, structures::bytes::GLOBAL_BINARY_DATA};
+use crate::{
+    Abstract, Byte, BytesStructure, Object, Structure, structures::bytes::GLOBAL_BINARY_DATA,
+};
 
 #[test]
-pub fn from_parts() {
+fn from_parts() {
     assert_eq!(BytesStructure::from_parts(&[], &[]), None);
 
-    {
-        let count = GLOBAL_BINARY_DATA.len();
-
-        {
-            let bytes = BytesStructure::from_parts(&[0, 1, 2], &[]).unwrap();
-            assert_eq!(Arc::strong_count(&bytes.data), 2);
-            assert_eq!(bytes.data.as_ref(), &[0, 1, 2]);
-
-            assert_eq!(GLOBAL_BINARY_DATA.len(), count + 1);
-        }
-
-        assert_eq!(GLOBAL_BINARY_DATA.len(), count);
-    }
+    let bytes = BytesStructure::from_parts(&[0, 1, 2], &[]).unwrap();
+    assert_eq!(bytes.data.as_ref(), &[0, 1, 2]);
 
     let bytes = BytesStructure::from_parts(&[], &[3, 4, 5]).unwrap();
     assert_eq!(bytes.data.as_ref(), &[3, 4, 5]);
 
     let bytes = BytesStructure::from_parts(&[0, 1, 2], &[3, 4, 5]).unwrap();
     assert_eq!(bytes.data.as_ref(), &[0, 1, 2, 3, 4, 5]);
+}
+
+#[test]
+fn parts() {
+    let bytes = BytesStructure::from_parts(&[42, 67, 69], &[]).unwrap();
+
+    assert_eq!(bytes.parts(), (Byte(42), [67, 69].as_slice()));
+}
+
+#[test]
+fn has() {
+    assert!(!BytesStructure::new(&[10]).unwrap().has(
+        &Object::Abstract(Abstract::BIT_0),
+        &Object::Abstract(Abstract(435784))
+    ));
+
+    assert!(!BytesStructure::new(&[10]).unwrap().has(
+        &Object::Abstract(Abstract::LIST_ITEM),
+        &Object::Abstract(Abstract(435784))
+    ));
+
+    assert!(!BytesStructure::new(&[10]).unwrap().has(
+        &Object::Abstract(Abstract::LIST_TAIL),
+        &Object::Abstract(Abstract(435784))
+    ));
+
+    assert!(BytesStructure::new(&[42]).unwrap().has(
+        &Object::Abstract(Abstract::LIST_ITEM),
+        &Object::Structure(Structure::Byte(Byte(42)))
+    ));
+
+    assert!(BytesStructure::new(&[42, 69]).unwrap().has(
+        &Object::Abstract(Abstract::LIST_TAIL),
+        &Object::Structure(Structure::Bytes(BytesStructure::new(&[69]).unwrap()))
+    ));
+
+    assert!(BytesStructure::new(&[42]).unwrap().has(
+        &Object::Abstract(Abstract::LIST_TAIL),
+        &Object::Structure(Structure::Empty)
+    ));
+}
+
+#[test]
+fn drop() {
+    let count = GLOBAL_BINARY_DATA.len();
+
+    {
+        let bytes = BytesStructure::from_parts(&[0, 1, 2], &[]).unwrap();
+        assert_eq!(Arc::strong_count(&bytes.data), 2);
+
+        assert_eq!(GLOBAL_BINARY_DATA.len(), count + 1);
+    }
+
+    assert_eq!(GLOBAL_BINARY_DATA.len(), count);
 }
